1de307ee2c330f0f4ecf88c62ac6ade8b212a45b
Yashodhan Kocharekar <ykochare@tibco.com>
2016-05-20 19:41:56 +0530

    changes 0.0.0.0 to ""

--- priam/src/main/java/com/netflix/priam/utils/TuneCassandra.java
+++ priam/src/main/java/com/netflix/priam/utils/TuneCassandra.java
@@ -47,7 +47,7 @@ public class TuneCassandra extends Task
     	
     	while (!isDone) {
     	  try {
-              tuner.writeAllProperties(config.getYamlLocation(), "0.0.0.0", config.getSeedProviderName());
+              tuner.writeAllProperties(config.getYamlLocation(), "", config.getSeedProviderName());
               isDone = true;
     	   } catch (IOException e) {
     		  LOGGER.info("Fail wrting cassandra.yml file. Retry again!");
